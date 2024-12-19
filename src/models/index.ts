ff8c75821c8adbebc55985ad4542602d4e742cb1
aldenzho <162027669+aldenzho@users.noreply.github.com>
2024-12-19 16:42:48 +0700

    Update index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || 'postgrexx2',
   {
     host: process.env.DB_HOST || 'localhost',
-    dialect: 'postgres',
+    dialect: 'postgres'
   }
 );
 
