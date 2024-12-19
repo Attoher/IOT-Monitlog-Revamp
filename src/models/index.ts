0edb92301895c0c58491f7320094a913108a2085
aldenzho <162027669+aldenzho@users.noreply.github.com>
2024-12-19 16:14:01 +0700

    Update index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,39 +1,14 @@
 import { Sequelize } from 'sequelize';
 import config from '../config/config';
 
-
-interface Config {
-  database: string;
-  username: string;
-  password: string;
-  host: string;
-  dialect: any; 
-}
-
-
-const dbConfig: Config = {
-  database: config.database || '',
-  username: config.username || '',
-  password: config.password || '',
-  host: config.host || 'localhost',
-  dialect: config.dialect || 'mysql',
-};
-
-// Initialize Sequelize
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-  host: dbConfig.host,
-  dialect: dbConfig.dialect,
-  logging: false, 
-});
-
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Database connection established successfully.');
-  })
-  .catch((error) => {
-    console.error('Unable to connect to the database:', error);
-  });
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'todolist',
+  process.env.DB_USER || 'postgres',
+  process.env.DB_PASSWORD || 'postgrexx2',
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+  }
+);
 
 export default sequelize;
